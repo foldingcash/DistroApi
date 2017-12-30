08b3b9888932249fed689dc2d3dfc4794a8e7634
Miguel <Miguel@TENACIOUSM>
2017-12-30 00:20:32 -0500

    Move to using Castle and application configuration file to manage the WCF service. The configuration is for http

---
+++ StatsDownload/StatsDownload.FileServer.TestHarness/CastleWindsor/DependencyInstaller.cs
@@ -0,0 +1,16 @@
+﻿namespace StatsDownload.FileServer.TestHarness
+{
+    using Castle.Facilities.WcfIntegration;
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+
+    public class DependencyInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<ITestHarnessFileServer>().ImplementedBy<TestHarnessFileServer>().AsWcfService());
+        }
+    }
+}
