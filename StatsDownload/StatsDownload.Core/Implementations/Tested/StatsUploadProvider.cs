57783c55499af25f502fb3515d5e36cc004bd103
Miguel <Miguel@TENACIOUSM>
2018-03-01 21:21:33 -0500

    Add the process for uploading the download file.

--- StatsDownload/StatsDownload.Core/Implementations/Tested/StatsUploadProvider.cs
+++ StatsDownload/StatsDownload.Core/Implementations/Tested/StatsUploadProvider.cs
@@ -1,18 +1,23 @@
 ﻿namespace StatsDownload.Core
 {
     using System;
+    using System.Collections.Generic;
 
     public class StatsUploadProvider : IStatsUploadService
     {
         private readonly IStatsUploadLoggingService loggingService;
 
+        private readonly IStatsFileParserService statsFileParserService;
+
         private readonly IStatsUploadDataStoreService statsUploadDataStoreService;
 
         public StatsUploadProvider(IStatsUploadDataStoreService statsUploadDataStoreService,
-                                   IStatsUploadLoggingService loggingService)
+                                   IStatsUploadLoggingService loggingService,
+                                   IStatsFileParserService statsFileParserService)
         {
             this.statsUploadDataStoreService = statsUploadDataStoreService;
             this.loggingService = loggingService;
+            this.statsFileParserService = statsFileParserService;
         }
 
         public StatsUploadResult UploadStatsFile()
@@ -23,6 +28,23 @@
                 if (DataStoreUnavailable())
                 {
                     return new StatsUploadResult(FailedReason.DataStoreUnavailable);
+                }
+
+                List<int> uploadFiles = statsUploadDataStoreService.GetDownloadsReadyForUpload();
+
+                foreach (int uploadFile in uploadFiles)
+                {
+                    loggingService.LogVerbose($"Starting stats file upload. DownloadId: {uploadFile}");
+                    statsUploadDataStoreService.StartStatsUpload(uploadFile);
+                    string fileData = statsUploadDataStoreService.GetFileData(uploadFile);
+                    List<UserData> usersData = statsFileParserService.Parse(fileData);
+
+                    foreach (UserData userData in usersData)
+                    {
+                        statsUploadDataStoreService.AddUserData(userData);
+                    }
+                    statsUploadDataStoreService.StatsUploadFinished(uploadFile);
+                    loggingService.LogVerbose($"Finished stats file upload. DownloadId: {uploadFile}");
                 }
 
                 return new StatsUploadResult();
