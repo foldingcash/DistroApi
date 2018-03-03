4282ba173cd55bcfb359a149cdee9cf25fe83e27
Miguel <Miguel@TENACIOUSM>
2018-03-03 09:15:04 -0500

    Parse the stats file into an object in memory

---
+++ StatsDownload/StatsDownload.Core.Tests/TestStatsFileParserProvider.cs
@@ -0,0 +1,83 @@
+﻿namespace StatsDownload.Core.Tests
+{
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestStatsFileParserProvider
+    {
+        private const string GoodStatsFile = @"Tue Dec 26 10:20:01 PST 2017
+name	newcredit	sum(total)	team
+PS3EdOlkkola	25882218711	458785	224497
+war	20508731397	544139	37651
+msi_TW	15889476570	359312	31403
+anonymous	13937689581	64221589	0
+TheWasp	13660834951	734045	70335";
+
+        private const string MalformedHeader = @"Tue Dec 26 10:20:01 PST 2017
+a malformed header not exactly matching the good stats file header
+PS3EdOlkkola	25882218711	458785	224497
+war	20508731397	544139	37651
+msi_TW	15889476570	359312	31403
+anonymous	13937689581	64221589	0
+TheWasp	13660834951	734045	70335";
+
+        private const string MalformedUserRecord = @"Tue Dec 26 10:20:01 PST 2017
+name	newcredit	sum(total)	team
+PS3EdOlkkola	25882218711	458785	224497
+war	20508731397	544139	37651
+msi_TW	15889476570	359312	31403
+a bad user record exists
+anonymous	not an ulong	64221589	0
+anonymous	13937689581	not an int	0
+anonymous	13937689581	64221589	not an int
+TheWasp	13660834951	734045	70335";
+
+        private IStatsFileParserService systemUnderTest;
+
+        [Test]
+        public void Parse_WhenInvoked_ReturnsListOfUsersData()
+        {
+            List<UserData> actual = systemUnderTest.Parse(GoodStatsFile);
+
+            Assert.That(actual.Count, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Parse_WhenInvoked_ReturnsParsedUserData()
+        {
+            List<UserData> usersData = systemUnderTest.Parse(GoodStatsFile);
+            UserData actual = usersData[2];
+
+            Assert.That(actual.Name, Is.EqualTo("msi_TW"));
+            Assert.That(actual.TotalPoints, Is.EqualTo(15889476570));
+            Assert.That(actual.TotalWorkUnits, Is.EqualTo(359312));
+            Assert.That(actual.TeamNumber, Is.EqualTo(31403));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(MalformedHeader)]
+        public void Parse_WhenInvokedWithFileWithMalformedHeader_ReturnsEmptyUserDataList(string fileData)
+        {
+            List<UserData> actual = systemUnderTest.Parse(fileData);
+
+            Assert.That(actual.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Parse_WhenInvokedWithMalformedUserRecord_SkipsMalformedUserRecord()
+        {
+            List<UserData> actual = systemUnderTest.Parse(MalformedUserRecord);
+
+            Assert.That(actual.Count, Is.EqualTo(4));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            systemUnderTest = new StatsFileParserProvider();
+        }
+    }
+}
---
+++ StatsDownload/StatsDownload.Core/Implementations/Tested/StatsFileParserProvider.cs
@@ -0,0 +1,49 @@
+﻿namespace StatsDownload.Core
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StatsFileParserProvider : IStatsFileParserService
+    {
+        private const string ExpectedHeader = @"name	newcredit	sum(total)	team";
+
+        public List<UserData> Parse(string fileData)
+        {
+            var usersData = new List<UserData>();
+
+            string[] fileLines = fileData?.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (fileLines == null || fileLines.Length < 3 || fileLines[1] != ExpectedHeader)
+            {
+                return usersData;
+            }
+
+            for (var lineIndex = 2; lineIndex < fileLines.Length; lineIndex++)
+            {
+                string[] unparsedUserData = fileLines[lineIndex].Split(new[] { '\t' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (unparsedUserData.Length != 4)
+                {
+                    continue;
+                }
+
+                string name = unparsedUserData[0];
+                ulong totalPoints;
+                int totalWorkUnits;
+                int teamNumber;
+
+                if (!ulong.TryParse(unparsedUserData[1], out totalPoints)
+                    || !int.TryParse(unparsedUserData[2], out totalWorkUnits)
+                    || !int.TryParse(unparsedUserData[3], out teamNumber))
+                {
+                    continue;
+                }
+
+                usersData.Add(new UserData(name, totalPoints, totalWorkUnits, teamNumber));
+            }
+
+            return usersData;
+        }
+    }
+}
