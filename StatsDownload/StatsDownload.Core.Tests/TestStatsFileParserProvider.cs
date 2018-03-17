ce86d1034072e6a4dcbdcb92e8b775ebccb845fc
Miguel <Miguel@TENACIOUSM>
2018-03-17 19:08:15 -0400

    Throw an exception when the file does not use the expected date time and header formats

--- StatsDownload/StatsDownload.Core.Tests/TestStatsFileParserProvider.cs
+++ StatsDownload/StatsDownload.Core.Tests/TestStatsFileParserProvider.cs
@@ -9,7 +9,18 @@
     [TestFixture]
     public class TestStatsFileParserProvider
     {
+        private const string EmptyStatsFile = @"Tue Dec 26 10:20:01 PST 2017
+name	newcredit	sum(total)	team";
+
         private const string GoodStatsFile = @"Tue Dec 26 10:20:01 PST 2017
+name	newcredit	sum(total)	team
+PS3EdOlkkola	25882218711	458785	224497
+war	20508731397	544139	37651
+msi_TW	15889476570	359312	31403
+anonymous	13937689581	64221589	0
+TheWasp	13660834951	734045	70335";
+
+        private const string MalformedDateTime = @"a malformed date time not exactly matching the good stats file header
 name	newcredit	sum(total)	team
 PS3EdOlkkola	25882218711	458785	224497
 war	20508731397	544139	37651
@@ -51,12 +62,13 @@ TheWasp	13660834951	734045	70335";
             }
         }
 
-        [Test]
-        public void Parse_WhenInvoked_ReturnsListOfUsersData()
+        [TestCase(GoodStatsFile, 5)]
+        [TestCase(EmptyStatsFile, 0)]
+        public void Parse_WhenInvoked_ReturnsListOfUsersData(string fileData, int expectedCount)
         {
-            List<UserData> actual = systemUnderTest.Parse(GoodStatsFile);
+            List<UserData> actual = systemUnderTest.Parse(fileData);
 
-            Assert.That(actual.Count, Is.EqualTo(5));
+            Assert.That(actual.Count, Is.EqualTo(expectedCount));
         }
 
         [Test]
@@ -74,11 +86,10 @@ TheWasp	13660834951	734045	70335";
         [TestCase(null)]
         [TestCase("")]
         [TestCase(MalformedHeader)]
-        public void Parse_WhenInvokedWithFileWithMalformedHeader_ReturnsEmptyUserDataList(string fileData)
+        [TestCase(MalformedDateTime)]
+        public void Parse_WhenInvokedWithFileWithMalformedHeader_ThrowsInvalidStatsFileException(string fileData)
         {
-            List<UserData> actual = systemUnderTest.Parse(fileData);
-
-            Assert.That(actual.Count, Is.EqualTo(0));
+            Assert.Throws<InvalidStatsFileException>(() => systemUnderTest.Parse(fileData));
         }
 
         [Test]
