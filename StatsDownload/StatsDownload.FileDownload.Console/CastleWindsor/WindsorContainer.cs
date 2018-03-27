1d431ba3b3dfb11e19a63a1160b8b671289f7591
Miguel <Miguel@TENACIOUSM>
2018-03-26 21:54:47 -0400

    Initial check-in of the console applications for the file download and stats upload

---
+++ StatsDownload/StatsDownload.FileDownload.Console/CastleWindsor/WindsorContainer.cs
@@ -0,0 +1,41 @@
+﻿namespace StatsDownload.FileDownload.Console
+{
+    using Castle.Facilities.TypedFactory;
+    using Castle.Windsor;
+
+    using CastleWindsorContainer = Castle.Windsor.WindsorContainer;
+
+    internal static class WindsorContainer
+    {
+        private static readonly object sync = new object();
+
+        private static IWindsorContainer innerContainer;
+
+        internal static IWindsorContainer Instance
+        {
+            get
+            {
+                lock (sync)
+                {
+                    return innerContainer ?? (innerContainer = CreateWindsorContainer());
+                }
+            }
+        }
+
+        public static void Dispose()
+        {
+            lock (sync)
+            {
+                innerContainer?.Dispose();
+                innerContainer = null;
+            }
+        }
+
+        private static IWindsorContainer CreateWindsorContainer()
+        {
+            var container = new CastleWindsorContainer();
+            container.AddFacility<TypedFactoryFacility>();
+            return container;
+        }
+    }
+}
---
+++ StatsDownload/StatsDownload.FileDownload.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StatsDownload.FileDownload.Console
+{
+    using StatsDownload.Core;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                DependencyRegistration.Register();
+
+                var service = WindsorContainer.Instance.Resolve<IFileDownloadService>();
+                service.DownloadStatsFile();
+            }
+            finally
+            {
+                WindsorContainer.Dispose();
+            }
+        }
+    }
+}
