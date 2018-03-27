9641ff9ea67cd83b763469a3394ab25353b7032a
Miguel <Miguel@TENACIOUSM>
2018-03-26 21:54:47 -0400

    Initial check-in of the console applications for the file download and stats upload

---
+++ StatsDownload/StatsDownload.FileDownload.Console/FileDownloadConsoleLoggingProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace StatsDownload.FileDownload.Console
+{
+    using StatsDownload.Logging;
+
+    public class FileDownloadConsoleLoggingProvider : IApplicationLoggingService
+    {
+        public void LogError(string message)
+        {
+        }
+
+        public void LogVerbose(string message)
+        {
+        }
+    }
+}
---
+++ StatsDownload/StatsDownload.StatsUpload.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StatsDownload.StatsUpload.Console
+{
+    using StatsDownload.Core;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                DependencyRegistration.Register();
+
+                var service = WindsorContainer.Instance.Resolve<IStatsUploadService>();
+                service.UploadStatsFiles();
+            }
+            finally
+            {
+                WindsorContainer.Dispose();
+            }
+        }
+    }
+}
