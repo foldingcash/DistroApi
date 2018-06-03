d02c75fa554ba9f2387e06186511978405cff637
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-06-03 18:05:14 -0400

    Add a filter for top one hundred to make dev integration testing fast in needed

---
+++ StatsDownload/StatsDownload.TestHarness/TestHarnessTopOneHundredUsersFilter.cs
@@ -0,0 +1,23 @@
+﻿namespace StatsDownload.TestHarness
+{
+    using System.Linq;
+
+    using StatsDownload.Core.Interfaces;
+    using StatsDownload.Core.Interfaces.DataTransfer;
+
+    public class TestHarnessTopOneHundredUsersFilter : IStatsFileParserService
+    {
+        private readonly IStatsFileParserService innerService;
+
+        public TestHarnessTopOneHundredUsersFilter(IStatsFileParserService innerService)
+        {
+            this.innerService = innerService;
+        }
+
+        public ParseResults Parse(string fileData)
+        {
+            ParseResults results = innerService.Parse(fileData);
+            return new ParseResults(results.UsersData.Take(100), results.FailedUsersData);
+        }
+    }
+}
