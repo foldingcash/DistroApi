7d54a24f74b17fe0bd31eaa5fd43276970cec228
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-06-04 01:49:57 -0400

    Add enable/disable for zero point users filter

--- StatsDownload/StatsDownload.Core.Tests/TestZeroPointUsersFilter.cs
+++ StatsDownload/StatsDownload.Core.Tests/TestZeroPointUsersFilter.cs
@@ -1,40 +1,56 @@
-﻿namespace StatsDownload.Core.Tests
+﻿using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using StatsDownload.Core.Implementations.Tested;
+using StatsDownload.Core.Interfaces;
+using StatsDownload.Core.Interfaces.DataTransfer;
+
+namespace StatsDownload.Core.Tests
 {
-    using System.Linq;
-
-    using NSubstitute;
-
-    using NUnit.Framework;
-
-    using StatsDownload.Core.Implementations.Tested;
-    using StatsDownload.Core.Interfaces;
-    using StatsDownload.Core.Interfaces.DataTransfer;
-
     [TestFixture]
     public class TestZeroPointUsersFilter
     {
-        private IStatsFileParserService innerServiceMock;
-
-        private IStatsFileParserService systemUnderTest;
-
-        [Test]
-        public void Parse_WhenInvoked_FiltersResults()
-        {
-            innerServiceMock.Parse("fileData")
-                            .Returns(new ParseResults(new[] { new UserData(), new UserData(null, 1, 0, 0) },
-                                new[] { new FailedUserData() }));
-
-            ParseResults actual = systemUnderTest.Parse("fileData");
-
-            Assert.That(actual.UsersData.Count(data => data.TotalPoints == 0), Is.EqualTo(0));
-        }
-
         [SetUp]
         public void SetUp()
         {
             innerServiceMock = Substitute.For<IStatsFileParserService>();
 
-            systemUnderTest = new ZeroPointUsersFilter(innerServiceMock);
+            settingsMock = Substitute.For<IZeroPointUsersFilterSettings>();
+
+            systemUnderTest = new ZeroPointUsersFilter(innerServiceMock, settingsMock);
+        }
+
+        private IZeroPointUsersFilterSettings settingsMock;
+
+        private IStatsFileParserService innerServiceMock;
+
+        private IStatsFileParserService systemUnderTest;
+
+        [Test]
+        public void Parse_WhenDisabled_DoesNotModifyResults()
+        {
+            settingsMock.Enabled.Returns(false);
+
+            var expected = new ParseResults(null, null);
+            innerServiceMock.Parse("fileData").Returns(expected);
+
+            var actual = systemUnderTest.Parse("fileData");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Parse_WhenInvoked_FiltersResults()
+        {
+            settingsMock.Enabled.Returns(true);
+
+            innerServiceMock.Parse("fileData")
+                            .Returns(new ParseResults(new[] {new UserData(), new UserData(null, 1, 0, 0)},
+                                new[] {new FailedUserData()}));
+
+            ParseResults actual = systemUnderTest.Parse("fileData");
+
+            Assert.That(actual.UsersData.Count(data => data.TotalPoints == 0), Is.EqualTo(0));
         }
     }
 }
--- StatsDownload/StatsDownload.Core/Implementations/Tested/ZeroPointUsersFilter.cs
+++ StatsDownload/StatsDownload.Core/Implementations/Tested/ZeroPointUsersFilter.cs
@@ -1,23 +1,29 @@
-﻿namespace StatsDownload.Core.Implementations.Tested
+﻿using System.Linq;
+using StatsDownload.Core.Interfaces;
+using StatsDownload.Core.Interfaces.DataTransfer;
+
+namespace StatsDownload.Core.Implementations.Tested
 {
-    using System.Linq;
-
-    using StatsDownload.Core.Interfaces;
-    using StatsDownload.Core.Interfaces.DataTransfer;
-
     public class ZeroPointUsersFilter : IStatsFileParserService
     {
         private readonly IStatsFileParserService innerService;
+        private readonly IZeroPointUsersFilterSettings settings;
 
-        public ZeroPointUsersFilter(IStatsFileParserService innerService)
+        public ZeroPointUsersFilter(IStatsFileParserService innerService, IZeroPointUsersFilterSettings settings)
         {
             this.innerService = innerService;
+            this.settings = settings;
         }
 
         public ParseResults Parse(string fileData)
         {
-            ParseResults results = innerService.Parse(fileData);
-            return new ParseResults(results.UsersData.Where(data => data.TotalPoints > 0), results.FailedUsersData);
+            if (settings.Enabled)
+            {
+                ParseResults results = innerService.Parse(fileData);
+                return new ParseResults(results.UsersData.Where(data => data.TotalPoints > 0), results.FailedUsersData);
+            }
+
+            return innerService.Parse(fileData);
         }
     }
 }
