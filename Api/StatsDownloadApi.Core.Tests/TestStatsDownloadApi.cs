f46fd1ede49d1d448a8f2464d3936ede11b88057
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-07-03 12:11:12 -0400

    Add API solution and test project to validate tests are running.

---
+++ Api/StatsDownloadApi.Core.Tests/TestStatsDownloadApi.cs
@@ -0,0 +1,24 @@
+﻿namespace StatsDownloadApi.Core.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestStatsDownloadApi
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            systemUnderTest = new StatsDownloadApi();
+        }
+
+        private StatsDownloadApi systemUnderTest;
+
+        [Test]
+        public void ReturnTrue_WhenInvoked_ReturnsTrue()
+        {
+            var actual = systemUnderTest.ReturnTrue();
+
+            Assert.That(actual, Is.True);
+        }
+    }
+}
