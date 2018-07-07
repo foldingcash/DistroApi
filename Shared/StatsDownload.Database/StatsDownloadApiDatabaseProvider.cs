89f953ec66c2b25e30a26263fd3c115052c4cd69
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-07-07 10:29:47 -0400

    Implement missing interface members

--- Shared/StatsDownload.Database/StatsDownloadApiDatabaseProvider.cs
+++ Shared/StatsDownload.Database/StatsDownloadApiDatabaseProvider.cs
@@ -1,7 +1,9 @@
 ﻿namespace StatsDownload.Database
 {
     using System;
+    using System.Collections.Generic;
     using Core.Interfaces;
+    using Core.Interfaces.DataTransfer;
 
     public class StatsDownloadApiDatabaseProvider : IStatsDownloadApiDatabaseService
     {
@@ -11,6 +13,11 @@
         {
             this.statsDownloadDatabaseService = statsDownloadDatabaseService ??
                                                 throw new ArgumentNullException(nameof(statsDownloadDatabaseService));
+        }
+
+        public IList<DistroUser> GetDistroUsers()
+        {
+            throw new NotImplementedException();
         }
 
         public bool IsAvailable()
