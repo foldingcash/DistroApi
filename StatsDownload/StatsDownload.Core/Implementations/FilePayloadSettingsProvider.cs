955745cda6a7d7c847b5ff58571a0bb0326bf2a9
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-08-27 15:18:20 -0400

    Use inline variable declaration for out variables

--- StatsDownload/StatsDownload.Core/Implementations/FilePayloadSettingsProvider.cs
+++ StatsDownload/StatsDownload.Core/Implementations/FilePayloadSettingsProvider.cs
@@ -113,29 +113,25 @@
             string unsafeAcceptAnySslCert = GetAcceptAnySslCert();
             string unsafeMinimumWaitTimeInHours = GetMinimumWaitTimeInHours();
 
-            Uri downloadUri;
-            if (!TryParseDownloadUri(unsafeDownloadUri, out downloadUri))
+            if (!TryParseDownloadUri(unsafeDownloadUri, out Uri downloadUri))
             {
                 throw NewFileDownloadArgumentException("Download Uri is invalid");
             }
 
-            int timeoutInSeconds;
-            if (!TryParseTimeout(downloadTimeout, out timeoutInSeconds))
+            if (!TryParseTimeout(downloadTimeout, out int timeoutInSeconds))
             {
                 timeoutInSeconds = 100;
                 loggingService.LogVerbose("The download timeout configuration was invalid, using the default value.");
             }
 
-            bool acceptAnySslCert;
-            if (!TryParseAcceptAnySslCert(unsafeAcceptAnySslCert, out acceptAnySslCert))
+            if (!TryParseAcceptAnySslCert(unsafeAcceptAnySslCert, out bool acceptAnySslCert))
             {
                 acceptAnySslCert = false;
                 loggingService.LogVerbose(
                     "The accept any SSL cert configuration was invalid, using the default value.");
             }
 
-            TimeSpan minimumWaitTimeSpan;
-            if (!TryParseMinimumWaitTimeSpan(unsafeMinimumWaitTimeInHours, out minimumWaitTimeSpan))
+            if (!TryParseMinimumWaitTimeSpan(unsafeMinimumWaitTimeInHours, out TimeSpan minimumWaitTimeSpan))
             {
                 minimumWaitTimeSpan = MinimumWait.TimeSpan;
                 loggingService.LogVerbose("The minimum wait time configuration was invalid, using the default value.");
