c0dbeb824cf3267a34da81ae24386b108fbd16fa
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-11-08 12:36:24 -0500

    Parse the date time formats from the configured settings and union it with the hard-coded formats

--- StatsDownload/StatsDownload.Core.Tests/TestStatsFileDateTimeFormatsAndOffsetProvider.cs
+++ StatsDownload/StatsDownload.Core.Tests/TestStatsFileDateTimeFormatsAndOffsetProvider.cs
@@ -1,7 +1,9 @@
 ﻿namespace StatsDownload.Core.Tests
 {
+    using System;
     using Implementations;
     using Interfaces;
+    using NSubstitute;
     using NUnit.Framework;
 
     [TestFixture]
@@ -10,17 +12,54 @@
         [SetUp]
         public void SetUp()
         {
-            systemUnderTest = new StatsFileDateTimeFormatsAndOffsetProvider();
+            statsFileDateTimeFormatsAndOffsetSettingsMock =
+                Substitute.For<IStatsFileDateTimeFormatsAndOffsetSettings>();
+            statsFileDateTimeFormatsAndOffsetSettingsMock
+                .GetStatsFileTimeZoneAndOffsetSettings().Returns("ZONE1=-1;ZONE2=0;ZONE3=1;");
+
+            systemUnderTest =
+                NewStatsFileDateTimeFormatsAndOffsetProvider(statsFileDateTimeFormatsAndOffsetSettingsMock);
         }
+
+        private IStatsFileDateTimeFormatsAndOffsetSettings statsFileDateTimeFormatsAndOffsetSettingsMock;
 
         private IStatsFileDateTimeFormatsAndOffsetService systemUnderTest;
 
         [Test]
-        public void GetStatsFileDateTimeFormatsAndOffset_WhenInvoked_ReturnsDateTimeFormatsAndOffsetsConstants()
+        public void Constructor_WhenNullDependencyProvided_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                NewStatsFileDateTimeFormatsAndOffsetProvider(null));
+        }
+
+        [Test]
+        public void GetStatsFileDateTimeZoneAndOffset_WhenInvoked_ReturnsDateTimeFormatsAndOffsetsConstants()
         {
             (string format, int hourOffset)[] actual = systemUnderTest.GetStatsFileDateTimeFormatsAndOffset();
 
             CollectionAssert.IsSupersetOf(actual, Constants.StatsFile.DateTimeFormatsAndOffset);
+        }
+
+        [Test]
+        public void GetStatsFileDateTimeZoneAndOffsetSettings_WhenInvoked_ReturnsConfiguredDateTimeFormatsAndOffsets()
+        {
+            (string format, int hourOffset)[] actual = systemUnderTest.GetStatsFileDateTimeFormatsAndOffset();
+
+            CollectionAssert.IsSupersetOf(actual, new[]
+            {
+                ("ddd MMM  d HH:mm:ss ZONE1 yyyy", -1),
+                ("ddd MMM dd HH:mm:ss ZONE1 yyyy", -1),
+                ("ddd MMM  d HH:mm:ss ZONE2 yyyy", 0),
+                ("ddd MMM dd HH:mm:ss ZONE2 yyyy", 0),
+                ("ddd MMM  d HH:mm:ss ZONE3 yyyy", 1),
+                ("ddd MMM dd HH:mm:ss ZONE3 yyyy", 1)
+            });
+        }
+
+        private IStatsFileDateTimeFormatsAndOffsetService NewStatsFileDateTimeFormatsAndOffsetProvider(
+            IStatsFileDateTimeFormatsAndOffsetSettings statsFileDateTimeFormatsAndOffsetSettings)
+        {
+            return new StatsFileDateTimeFormatsAndOffsetProvider(statsFileDateTimeFormatsAndOffsetSettings);
         }
     }
 }
--- StatsDownload/StatsDownload.Core/Implementations/StatsFileDateTimeFormatsAndOffsetProvider.cs
+++ StatsDownload/StatsDownload.Core/Implementations/StatsFileDateTimeFormatsAndOffsetProvider.cs
@@ -1,12 +1,50 @@
 ﻿namespace StatsDownload.Core.Implementations
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Interfaces;
 
     public class StatsFileDateTimeFormatsAndOffsetProvider : IStatsFileDateTimeFormatsAndOffsetService
     {
+        private readonly IStatsFileDateTimeFormatsAndOffsetSettings statsFileDateTimeFormatsAndOffsetSettings;
+
+        public StatsFileDateTimeFormatsAndOffsetProvider(
+            IStatsFileDateTimeFormatsAndOffsetSettings statsFileDateTimeFormatsAndOffsetSettings)
+        {
+            this.statsFileDateTimeFormatsAndOffsetSettings = statsFileDateTimeFormatsAndOffsetSettings ??
+                                                             throw new ArgumentNullException(
+                                                                 nameof(statsFileDateTimeFormatsAndOffsetSettings));
+        }
+
         public (string format, int hourOffset)[] GetStatsFileDateTimeFormatsAndOffset()
         {
-            return Constants.StatsFile.DateTimeFormatsAndOffset;
+            (string format, int hourOffset)[] configuredFormatsAndOffset = GetConfiguredFormatsAndOffset();
+
+            return Constants.StatsFile.DateTimeFormatsAndOffset.Union(configuredFormatsAndOffset).ToArray();
+        }
+
+        private (string format, int hourOffset)[] GetConfiguredFormatsAndOffset()
+        {
+            string settings = statsFileDateTimeFormatsAndOffsetSettings.GetStatsFileTimeZoneAndOffsetSettings();
+
+            string[] settingsSplit = settings.Split(';', '=');
+
+            var configuredFormatsAndOffset = new List<(string format, int hourOffset)>();
+
+            for (var index = 0; index < settingsSplit.Length && index + 1 < settingsSplit.Length; index++)
+            {
+                string timeZone = settingsSplit[index];
+                index++;
+                string offset = settingsSplit[index];
+
+                foreach (string dateTimeFormat in Constants.StatsFile.DateTimeFormats)
+                {
+                    configuredFormatsAndOffset.Add((string.Format(dateTimeFormat, timeZone), int.Parse(offset)));
+                }
+            }
+
+            return configuredFormatsAndOffset.ToArray();
         }
     }
 }
