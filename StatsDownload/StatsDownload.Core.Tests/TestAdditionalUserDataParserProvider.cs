1f9d825633b9026810114e50ab2b10138eb4f8dc
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-11-08 10:16:54 -0500

    Validate users parsing when using a dash in their friendly name. Only underscore will be used to separate friendly name, tag, and bitcoin address

--- StatsDownload/StatsDownload.Core.Tests/TestAdditionalUserDataParserProvider.cs
+++ StatsDownload/StatsDownload.Core.Tests/TestAdditionalUserDataParserProvider.cs
@@ -31,11 +31,7 @@
 
         [TestCase("Address")]
         [TestCase("Name_Address")]
-        [TestCase("Name.Address")]
-        [TestCase("Name-Address")]
         [TestCase("Name_TAG_Address")]
-        [TestCase("Name.TAG.Address")]
-        [TestCase("Name-TAG-Address")]
         [TestCase("Name__Address")]
         public void Parse_WhenInvoked_ReturnsBitcoinAddress(string name)
         {
@@ -46,12 +42,19 @@
             Assert.That(userData.BitcoinAddress, Is.EqualTo("Address"));
         }
 
+        [TestCase("Name.Name_Address")]
+        [TestCase("Name.Name_TAG_Address")]
+        public void Parse_WhenInvoked_ReturnsComplexFriendlyName(string name)
+        {
+            var userData = new UserData(0, name, 0, 0, 0);
+
+            systemUnderTest.Parse(userData);
+
+            Assert.That(userData.FriendlyName, Is.EqualTo("Name.Name"));
+        }
+
         [TestCase("Name_Address")]
-        [TestCase("Name.Address")]
-        [TestCase("Name-Address")]
         [TestCase("Name_TAG_Address")]
-        [TestCase("Name.TAG.Address")]
-        [TestCase("Name-TAG-Address")]
         [TestCase("Name__Address")]
         public void Parse_WhenInvoked_ReturnsFriendlyName(string name)
         {
@@ -64,7 +67,7 @@
 
         [TestCase("")]
         [TestCase(null)]
-        [TestCase("part1.part2.part3.part4")]
+        [TestCase("part1_part2_part3_part4")]
         public void Parse_WhenNoAdditionalUserData_ReturnsUserData(string name)
         {
             var userData = new UserData(0, name, 0, 0, 0);
@@ -77,11 +80,7 @@
 
         [TestCase("name")]
         [TestCase("Name_BadAddress")]
-        [TestCase("Name.BadAddress")]
-        [TestCase("Name-BadAddress")]
         [TestCase("Name_TAG_BadAddress")]
-        [TestCase("Name.TAG.BadAddress")]
-        [TestCase("Name-TAG-BadAddress")]
         public void Parse_WhenNoBitcoinAddress_ReturnsNoAdditionalData(string name)
         {
             var userData = new UserData(0, name, 0, 0, 0);
