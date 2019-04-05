fbe7070b8b156f72c25bbba582b1da5c54f45106
Miguel Molina Jr <mmolina@foldingcoin.net>
2019-04-04 21:10:46 -0400

    remove mysql because implementation is not complete

--- Shared/StatsDownload.Database.CastleWindsor/DatabaseFactoryComponentSelector.cs
+++ Shared/StatsDownload.Database.CastleWindsor/DatabaseFactoryComponentSelector.cs
@@ -18,10 +18,6 @@
         protected override string GetComponentName(MethodInfo method, object[] arguments)
         {
             string databaseType = settings.GetDatabaseType();
-            if (string.Equals(databaseType, "MySql", StringComparison.OrdinalIgnoreCase))
-            {
-                return typeof (MySqlDatabaseConnectionProvider).FullName;
-            }
 
             if (string.Equals(databaseType, "MicrosoftSql", StringComparison.OrdinalIgnoreCase))
             {
