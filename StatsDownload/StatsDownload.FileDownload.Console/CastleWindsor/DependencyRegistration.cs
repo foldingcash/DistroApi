cae9f0585a83ef4516cdbf578c340a4c87e7a375
Miguel Molina Jr <molim134@gmail.com>
2019-12-25 13:03:56 -0500

    call install on registration classes manually. upgrade dependency

--- StatsDownload/StatsDownload.FileDownload.Console/CastleWindsor/DependencyRegistration.cs
+++ StatsDownload/StatsDownload.FileDownload.Console/CastleWindsor/DependencyRegistration.cs
@@ -1,19 +1,10 @@
 ﻿namespace StatsDownload.FileDownload.Console.CastleWindsor
 {
-    using System.Reflection;
-
-    using Castle.MicroKernel.Registration;
-    using Castle.Windsor.Installer;
-
-    using StatsDownload.Extensions;
-
     internal static class DependencyRegistration
     {
-        private static string AssemblyDirectory => Assembly.GetExecutingAssembly().GetCodebaseDirectory();
-
         internal static void Register()
         {
-            WindsorContainer.Instance.Install(FromAssembly.InDirectory(new AssemblyFilter(AssemblyDirectory)));
+            WindsorContainer.Instance.Install(new DependencyInstaller());
         }
     }
 }
