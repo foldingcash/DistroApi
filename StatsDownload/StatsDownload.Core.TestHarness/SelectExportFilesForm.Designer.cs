86ce58365645189b2c1745615d92f6382d2ed423
Miguel Molina Jr <molim134@gmail.com>
2021-01-20 17:12:38 -0500

    Show icon on sub form

--- StatsDownload/StatsDownload.Core.TestHarness/SelectExportFilesForm.Designer.cs
+++ StatsDownload/StatsDownload.Core.TestHarness/SelectExportFilesForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectExportFilesForm));
             this.ExportFilesCheckedListBox = new System.Windows.Forms.CheckedListBox();
             this.FinishedButton = new System.Windows.Forms.Button();
             this.CancelButton = new System.Windows.Forms.Button();
@@ -36,16 +37,16 @@
             // ExportFilesCheckedListBox
             // 
             this.ExportFilesCheckedListBox.FormattingEnabled = true;
-            this.ExportFilesCheckedListBox.Location = new System.Drawing.Point(122, 12);
+            this.ExportFilesCheckedListBox.Location = new System.Drawing.Point(107, 11);
             this.ExportFilesCheckedListBox.Name = "ExportFilesCheckedListBox";
-            this.ExportFilesCheckedListBox.Size = new System.Drawing.Size(401, 463);
+            this.ExportFilesCheckedListBox.Size = new System.Drawing.Size(351, 418);
             this.ExportFilesCheckedListBox.TabIndex = 0;
             // 
             // FinishedButton
             // 
-            this.FinishedButton.Location = new System.Drawing.Point(12, 193);
+            this.FinishedButton.Location = new System.Drawing.Point(10, 181);
             this.FinishedButton.Name = "FinishedButton";
-            this.FinishedButton.Size = new System.Drawing.Size(104, 41);
+            this.FinishedButton.Size = new System.Drawing.Size(91, 38);
             this.FinishedButton.TabIndex = 1;
             this.FinishedButton.Text = "Finished";
             this.FinishedButton.UseVisualStyleBackColor = true;
@@ -53,9 +54,9 @@
             // 
             // CancelButton
             // 
-            this.CancelButton.Location = new System.Drawing.Point(12, 240);
+            this.CancelButton.Location = new System.Drawing.Point(10, 225);
             this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Size = new System.Drawing.Size(104, 41);
+            this.CancelButton.Size = new System.Drawing.Size(91, 38);
             this.CancelButton.TabIndex = 2;
             this.CancelButton.Text = "Cancel";
             this.CancelButton.UseVisualStyleBackColor = true;
@@ -63,17 +64,18 @@
             // 
             // SelectExportFilesForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(535, 486);
+            this.ClientSize = new System.Drawing.Size(468, 456);
             this.Controls.Add(this.CancelButton);
             this.Controls.Add(this.FinishedButton);
             this.Controls.Add(this.ExportFilesCheckedListBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SelectExportFilesForm";
-            this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Select Files to Export";
             this.ResumeLayout(false);
