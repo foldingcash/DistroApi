49e5b99ef266ae19e3457cfdec886803cd96489b
Miguel Molina Jr <molim134@gmail.com>
2021-01-21 10:14:46 -0500

    use DI, this is the way

--- StatsDownload/StatsDownload.Core.TestHarness/Program.cs
+++ StatsDownload/StatsDownload.Core.TestHarness/Program.cs
@@ -3,7 +3,11 @@ namespace StatsDownload.Core.TestHarness
     using System;
     using System.Windows.Forms;
 
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
     using StatsDownload.Core.TestHarness.CastleWindsor;
+    using StatsDownload.DependencyInjection;
 
     internal static class Program
     {
@@ -13,12 +17,21 @@ namespace StatsDownload.Core.TestHarness
         [STAThread]
         private static void Main()
         {
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+            {
+                services.ConfigureStatsDownload(context.Configuration);
+
+                services.AddSingleton<MainForm>();
+            });
+
+            IHost host = hostBuilder.Build();
+
             DependencyRegistration.Register();
             Application.ApplicationExit += (sender, args) => WindsorContainer.Dispose();
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            Application.Run(host.Services.GetRequiredService<MainForm>());
         }
     }
 }
