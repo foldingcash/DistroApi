7b8b709ed42440867df1297041af909e1dcfa7c1
Miguel Molina Jr. <molim134@gmail.com>
2024-08-18 19:15:49 -0400

    implement a POC for verify signed messages using Bitcoin

--- Api/StatsDownloadApi.WebApi/Program.cs
+++ Api/StatsDownloadApi.WebApi/Program.cs
@@ -2,11 +2,17 @@
 {
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using NBitcoin;
+    using NBitcoin.Networks;
+    using Stratis.Bitcoin.Utilities;
 
     public class Program
     {
         public static void Main(string[] args)
         {
+            var temp = new BitcoinPubKeyAddress("14WPq1y6Nd1QfMks17hNBTrbQsYoLswicz", new Stratis.Bitcoin.Networks.BitcoinMain());
+            var valid = temp.VerifyMessage("test", "HxWJlzbey8MAjdVH8qsopRgTX0m/4G47Gu8mw/Xwp4j6TcvwKgWzNCUQDuQV9LcdcsSy/y8BpDND6AxqpAiiARA=");
+
             BuildWebHost(args).Run();
         }
 
