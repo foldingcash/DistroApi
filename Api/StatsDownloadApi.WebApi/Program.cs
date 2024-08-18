42af87cd879271a792c93c83ce77450b496990cd
Miguel Molina Jr. <molim134@gmail.com>
2024-08-18 19:22:51 -0400

    implement a POC for verifying bitcoin messages

--- Api/StatsDownloadApi.WebApi/Program.cs
+++ Api/StatsDownloadApi.WebApi/Program.cs
@@ -2,11 +2,15 @@
 {
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using NBitcoin;
 
     public class Program
     {
         public static void Main(string[] args)
         {
+            var temp = new BitcoinPubKeyAddress("14WPq1y6Nd1QfMks17hNBTrbQsYoLswicz", Network.Main);
+            var valid = temp.VerifyMessage("test", "HxWJlzbey8MAjdVH8qsopRgTX0m/4G47Gu8mw/Xwp4j6TcvwKgWzNCUQDuQV9LcdcsSy/y8BpDND6AxqpAiiARA=");
+
             BuildWebHost(args).Run();
         }
 
